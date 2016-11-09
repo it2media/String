4d52502189823d8397c99bc1213326df78637028
malzma <markus.malz@it2media.de>
2016-11-09 08:37:34 +0100

    kickoff

---
+++ src/IT2media.Extensions.String/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IT2media.Extensions.String
+{
+    public static class StringExtensions
+    {
+        public static ushort GetHashCodeUShort(this string str)
+        {
+            return (ushort)str.GetHashCode();
+        }
+    }
+}
