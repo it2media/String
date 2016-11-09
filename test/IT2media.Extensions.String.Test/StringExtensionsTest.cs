60dd3002040403cb238f3240a90a9d35acce20a6
malzma <markus.malz@it2media.de>
2016-11-09 09:40:22 +0100

    simple test

---
+++ test/IT2media.Extensions.String.Test/StringExtensionsTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+using IT2media.Extensions.String;
+
+namespace Tests
+{
+    public class StringExtensionsTest
+    {
+        [Fact]
+        public void GetHashCodeUShort_ReturnsDifferentHashCodesOnDifferentStrings() 
+        {
+            var hash1 = "hallo".GetHashCodeUShort();
+            var hash2 = "halloo".GetHashCodeUShort();
+
+            Console.Write(hash1);
+            
+            Console.Write(hash2);
+
+            Assert.True(hash1 != hash2);
+        }
+    }
+}
