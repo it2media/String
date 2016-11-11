3e64c0cc37bdb18660e50275d0716d18d6cc73b1
malzma <markus.malz@it2media.de>
2016-11-11 12:51:43 +0100

    GetHashCode() used in CoreFX Release 1.1.0

--- src/IT2media.Extensions.String/StringExtensions.cs
+++ src/IT2media.Extensions.String/StringExtensions.cs
@@ -7,9 +7,35 @@ namespace IT2media.Extensions.String
 {
     public static class StringExtensions
     {
-        public static ushort GetHashCodeUShort(this string str)
+        public static ushort GetHashCodeUShort(this string s)
         {
-            return (ushort)str.GetHashCode();
+            return (ushort)GetHashCodeInternal(s);
+        }
+
+        private static int GetHashCodeInternal(string s)
+        {
+            // This hash code is a simplified version of 
+            // the GetHashCode() used in CoreFX Release 1.1.0 
+            // https://github.com/dotnet/corefx/blob/release/1.1.0/src/Common/src/System/Text/StringOrCharArray.cs
+
+            int count = s.Length;
+
+            int hash1 = (5381 << 16) + 5381;
+            int hash2 = hash1;
+
+            for (int i = 0; i < count; ++i)
+            {
+                int c = s[i];
+                hash1 = ((hash1 << 5) + hash1) ^ c;
+
+                if (++i >= count)
+                    break;
+
+                c = s[i];
+                hash2 = ((hash2 << 5) + hash2) ^ c;
+            }
+
+            return hash1 + (hash2 * 1566083941);
         }
     }
 }
